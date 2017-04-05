1519a05e46651df63a7659d73a3c31692cbcf8a0
Fourslash <fourslash@gmail.com>
2017-04-05 23:36:37 +0300

    add logging

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const APP_PORT = '3000';
 const APP_HOSTNAME = '0.0.0.0'
 
+const logger = require('morgan');
+
+const router = require('./app/router').router;
+
 const express = require('express');
 const app = express();
 
+app.use(logger('dev'));
+app.use('/', router);
 
 app.listen(APP_PORT, APP_HOSTNAME, () => {
     console.log(`Server started at ${APP_HOSTNAME}:${APP_PORT}`);
